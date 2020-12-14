1cfdae6a06f48ede6365cc32be1e435516bba03d
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2020-12-14 09:08:46 +0100

    Wykonano  zadanie 3.2.1–wysyłanie tekstu i liczby przez UART

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+uint8_t i = 0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("Witaj programisto!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  Serial.println(i);
+  delay(2000);
 }
