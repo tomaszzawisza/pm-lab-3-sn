f68e4ac0c17d36ca67344e4a0403f6e1340d67ea
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2020-12-14 10:41:46 +0100

    Wykonano zadanie 3.4.1–PWM rand

--- Zadanie-3-4/src/main.cpp
+++ Zadanie-3-4/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+#define PIN_G 5
+#define PIN_B 6
+#define PIN_R 9
+
+int r,g,b;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+  pinMode(PIN_R, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  analogWrite(PIN_R, rand());
+  analogWrite(PIN_G, rand());
+  analogWrite(PIN_B, rand());
+  delay(1000);
 }
