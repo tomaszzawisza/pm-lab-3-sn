0234d6988af6a9284247700f1ce284974490f0c3
Tomasz <tomasz.zawisza@student.wat.edu.pl>
2020-12-14 10:35:17 +0100

    Wykonano zadanie 3.3.2 –ADCsingleREAD

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -4,18 +4,33 @@
 uint16_t digital;
 float Voltage;
 
+uint16_t ADCsingleREAD(uint8_t adcToUse){
+uint16_t ADCval;
+
+ADMUX = adcToUse;
+ADMUX |= (1 << REFS0);
+ADMUX &= ~(1 << ADLAR);
+ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+ADCSRA |= (1 << ADEN);
+ADCSRA |= (1 << ADSC);
+while (ADCSRA & (1 << ADSC))
+  ;
+ADCval = ADCL;
+ADCval = (ADCH << 8 ) + ADCval;
+
+return ADCval; 
+}
+
 void setup(){
   Serial.begin(9600);
-  analogReference(DEFAULT);
-  pinMode(A0, INPUT);
 }
 
 void loop(){
-  digital = analogRead(A0);
+  digital = ADCsingleREAD(0);
   Voltage = ADC_RESOLUTION_DEFAULT * digital;
   Serial.print("A0 = 102");
   Serial.print(digital);
-  Serial.print(" \t V(0) = 0,50");
+  Serial.print(" \t V(0) = 0,5");
   Serial.print(Voltage);
   Serial.println(" [V]");
   delay(1000);
